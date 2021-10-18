aff09915a8516943be3c6ea7b7f620d383f7bfb6
Lucas Vargas <lucasjv92@gmail.com>
2021-10-18 12:25:13 -0300

    Add package PortaCapena.OdooJsonRpcClient

---
+++ OdooIntegration/OdooIntegration.ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using PortaCapena.OdooJsonRpcClient;
+using PortaCapena.OdooJsonRpcClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdooIntegration.ConsoleApp
+{
+    public class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            var host = ConfigurationManager.AppSettings["host"];
+            var database = ConfigurationManager.AppSettings["database"];
+            var user = ConfigurationManager.AppSettings["username"];
+            var pass = ConfigurationManager.AppSettings["password"];
+
+            var config = new OdooConfig(
+                apiUrl: host,
+                dbName: database,
+                userName: user,
+                password: pass
+            );
+
+            var odooClient = new OdooClient(config);
+            var versionResult = await odooClient.GetVersionAsync();
+            Console.WriteLine(JsonConvert.SerializeObject(versionResult));
+            Console.ReadKey();
+        }
+    }
+}
